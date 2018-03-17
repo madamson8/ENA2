ce682b44b426df8e40aac7faa9ee3ff9c841ce84
Matthew Adamson <matthewadamson8@gmail.com>
2018-03-17 16:35:17 -0600

    Pusing Broken Code

--- ena.py
+++ ena.py
@@ -1,28 +1,59 @@
 import psycopg2
 import sys
+from subprocess import call
+from pathlib import Path
 
-class ENA():
+
+class ENA(object):
     available_commands = [
             'help',
             'login'
     ]
 
-    def help():
-        for i in available_commands:
+    def run(self):
+        help()
+
+    def help(self):
+        for i in self.available_commands:
             print(i)
 
-    
-    def deploy():
-        print("Not ready yet.")
+    def exit(self):
+        print("Exiting")
+
+    def deploy(self):
+        yn = input("Do you have heroku installed? Y/n ")
+        if yn.lower() == "y":
+            print("Beginning Deployment.")
+            print("Creating siteinfo.html")
+            yn = input("I have detected your home as: " + str(Path.home()) + ".  Is this correct? Y/n ")
+            if yn.lower() == "y":
+                filepath = input("Please enter the path to the root of your project: ")
+                try:
+                    new_filepath = "" + filepath
+                    file = open(new_filepath, 'r')
+                except:
+                    pass
+                call("heroku login")
+        else:
+            print("Please install the Heroku CLI to contine.")
+            exit()
+
+#
+# def main():
+#     conn_string = "host='localhost' dbname='enabrain' user='postgres' password='BrightBridge1'"
+#     print("Connecting to a database\n ->")
+#     conn = psycopg2.connect(conn_string)
+#     cursor = conn.cursor()
+#     print("Connected!\n")
+#     conn.close()
+
+# if __name__ == "__main__":
+#     main()
+#     ena = ENA()
+#     ena.run()
 
 
-def main():
-    conn_string = "host='localhost' dbname='enabrain' user='postgres' password='BrightBridge1'"
-    print("Connecting to a database\n ->")
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    print("Connected!\n")
+ena = ENA()
+ena.run()
 
-if __name__ == "__main__":
-    main()
 
