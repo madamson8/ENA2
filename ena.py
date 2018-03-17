8b6359ed2a736a2bd0caea1329c551a1cacdd09e
Herbglitch <bighebe@gmail.com>
2018-03-17 16:36:17 -0600

    pass crash

--- ena.py
+++ ena.py
@@ -1,5 +1,10 @@
 import psycopg2
 import sys
+from passlib.hash import pbkdf2_sha256
+
+password = "asdf"
+pass_hash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
+
 
 class ENA():
     available_commands = [
@@ -11,9 +16,15 @@ class ENA():
         for i in available_commands:
             print(i)
 
-    
+
     def deploy():
         print("Not ready yet.")
+
+
+class password():
+    def authenticate_user(password):
+        print("hurray")
+        return pbkdf2_sha256.verify(passsword, pass_hash)
 
 
 def main():
@@ -22,7 +33,20 @@ def main():
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
     print("Connected!\n")
+    pass_class = password()
+    # test = input("pass:")
+    # n_test = pass_class.authenticate_user(test)
+    # print(n_test)
 
 if __name__ == "__main__":
     main()
 
+
+
+
+
+    # def login():
+    #     if(pass == inputpassword):
+    #         return True
+    #     else:
+    #         return False
